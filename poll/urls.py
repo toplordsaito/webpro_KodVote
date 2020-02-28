0f68c856f9ca8df4c02ef0449d2092617750c7ab
toplordsaito2 <61070214@kmitl.ac.th>
2020-02-28 22:50:34 +0700

    add choice

--- poll/urls.py
+++ poll/urls.py
@@ -4,6 +4,11 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('my/', views.user_poll, name='poll_user'),
-    path('delete/<int:poll_id>', views.deletePoll, name="poll_delete"),
+    path('view/<int:poll_id>/', views.viewPoll, name="poll_view"),
+    path('delete/<int:poll_id>/', views.deletePoll, name="poll_delete"),
+    path('update/<int:poll_id>/', views.updatePoll, name='poll_update'),
+    path('addChoice/<int:poll_id>/', views.createChoice, name='choice_create'),
+    path('deleteChoice/<int:choice_id>/',
+         views.deleteChoice, name='choice_delete'),
     path('create/', views.createPoll, name='poll_create'),
 ]
--- poll/views.py
+++ poll/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, HttpResponseRedirect, redirect
 from django.contrib.auth.decorators import login_required
-from .models import Poll
+from .models import Poll, Poll_Choice, Poll_Vote
 from datetime import datetime
 from time import time
 from dateutil.parser import parse
@@ -21,6 +21,7 @@ def user_poll(request):
 
 @login_required
 def createPoll(request):
+    context = {'title': 'Create Poll'}
     if request.method == 'POST':
         subject = request.POST.get('subject')
         detail = request.POST.get('detail')
@@ -28,18 +29,40 @@ def createPoll(request):
         end_date = checkDate(request.POST.get('end_date'))
         password = request.POST.get('password', None)
         picture = request.FILES.get('picture', None)
-        print(request.POST)
-
         # rename picture
         if picture:
             ext = '.' + picture.name.split('.')[-1]
             picture.name = str(time()) + ext
 
         poll = Poll.objects.create(subject=subject, detail=detail, start_date=start_date,
-                    end_date=end_date, password=password, create_by=request.user, picture=picture)
-        return redirect('home')
+                                   end_date=end_date, password=password, create_by=request.user, picture=picture)
+        context['title'] = "Update Poll"
+        context['msg'] = "Create Successfuly"
+        context['poll'] = poll
+    return render(request, 'poll/create.html', context)
 
-    return render(request, 'poll/create.html')
+
+@login_required
+def updatePoll(request, poll_id):
+    poll = Poll.objects.get(pk=poll_id)
+    context = {}
+    if request.method == 'POST' and poll.create_by == request.user:
+        poll.subject = request.POST.get('subject')
+        poll.detail = request.POST.get('detail')
+        poll.start_date = checkDate(request.POST.get('start_date'))
+        poll.end_date = checkDate(request.POST.get('end_date'))
+        poll.password = request.POST.get('password')
+        picture = request.FILES.get('picture', None)
+        if picture:
+            ext = '.' + picture.name.split('.')[-1]
+            picture.name = str(time()) + ext
+            poll.picture = picture
+        poll.save()
+        context['msg'] = "Update Successfuly"
+    context['title'] = 'Update Poll'
+    context['poll'] = poll
+    context['choices'] = Poll_Choice.objects.filter(poll_id=poll_id)
+    return render(request, 'poll/create.html', context)
 
 
 @login_required
@@ -48,6 +71,34 @@ def deletePoll(request, poll_id):
     if poll.create_by == request.user:
         poll.delete()
     return redirect(to='home')
+
+
+@login_required
+def createChoice(request, poll_id):
+    poll = Poll.objects.get(pk=poll_id)
+    if request.method == 'POST' and poll.create_by == request.user:
+        subject = request.POST.get('subject')
+        picture = request.FILES.get('picture', None)
+        if picture:
+            ext = '.' + picture.name.split('.')[-1]
+            picture.name = str(poll_id) + "_" + str(time()) + ext
+        choice = Poll_Choice.objects.create(
+            subject=subject, picture=picture, poll_id=poll)
+        return redirect(to='poll_update', poll_id=poll_id)
+    return redirect('home')
+
+
+@login_required
+def deleteChoice(request, choice_id):
+    choice = Poll_Choice.objects.get(pk=choice_id)
+    if choice.poll_id.create_by == request.user:
+        choice.delete()
+    return redirect(to='poll_update', poll_id=choice.poll_id.id)
+
+
+@login_required
+def viewPoll(request, poll_id):
+    return render(request, 'poll/view_poll.html')
 
 
 def checkDate(dateString):
